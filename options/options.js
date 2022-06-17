50520dd8cc442c36fd45ddab9a13ed2ee4d0b894
Crowdbotics <team@crowdbotics.com>
2022-06-17 18:46:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "ardent_credit_union_35776",
+  url: "https://ardent_credit_union_35776.botics.co",
+  api: "https://ardent_credit_union_35776.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
